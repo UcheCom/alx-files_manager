62c2173648609bd03ed31e5f59fc45593a3dc280
UcheCom <uche4newton1981@gmail.com>
2024-05-12 20:11:46 +0100

    utils updated

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
 
 // MongoDB client created
-const url = `mongodb://${host}:{port}`;
+const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
@@ -16,7 +16,7 @@ class DBClient {
         console.error(err);
       } else {
         this.db = this.client.db(database);
-        console.log(err);
+        console.log('Server connected');
       }
     });
    }
