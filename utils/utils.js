8b4bbedee919fd578a504890cdc47c060bf8a132
UcheCom <uche4newton1981@gmail.com>
2024-05-13 23:01:49 +0100

    Files Updated

---
+++ utils/utils.js
@@ -0,0 +1,23 @@
+export function getAuthHeader(request) {
+  const header = request.headers.authorization;
+  if (!header) return null;
+  return header;
+}
+
+export function getToken(authHeader) {
+  const tokenType = authHeader.substring(0, 5);
+  if (tokenType !== 'Basic') return null;
+  return authHeader.substring(6);
+}
+
+export function decodeToken(token) {
+  const decodedToken = Buffer.from(token, 'base64').toString('utf8');
+  if (!decodedToken.includes(':')) return null;
+  return decodedToken;
+}
+
+export function getCre(decodedToken) {
+  const [email, password] = decodedToken.split(':');
+  if (!email || !password) return null;
+  return email;
+}
