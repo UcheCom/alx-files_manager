33bac288549c122c8c536b2135e379b5c6a28d77
UcheCom <uche4newton1981@gmail.com>
2024-05-13 12:29:30 +0100

    Updated

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,14 +2,16 @@ import redisClient from '../utils/redis.js';
 import dbClient from '../utils/db.js';
 
 class AppController {
-  static getStatus(req, res) {
+  static async getStatus(req, res) {
     res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    res.end();
   }
 
   static async getStats(req, res) {
     const users= await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
     res.status(200).json({ users, files });
+    res.end();
   }
 }
 
